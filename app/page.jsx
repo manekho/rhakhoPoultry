3bf1ad79279a7c4043fa7a62a1cba92078a13bb6
learnease95 <learnease95@gmail.com>
2024-06-14 09:49:14 -0700

    added images

--- app/page.jsx
+++ app/page.jsx
@@ -3,20 +3,73 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.description_container}>
-        <p className={styles.description}>
-          All types of poultry, cattle and piggery feeds available at wholesale
-          price
-        </p>
+    <>
+      <div className={styles.container}>
+        <div className={styles.description_container}>
+          <p className={styles.description}>
+            All types of poultry, cattle and piggery feeds available at
+            wholesale price
+          </p>
+          <Image
+            className={styles.background_image}
+            src={"/hero.png"}
+            width={250}
+            height={250}
+          />
+          <button className={styles.check_button}>Check availability</button>
+        </div>
+      </div>
+      <div className={styles.products}>Products</div>
+      <div className={styles.image_container}>
         <Image
-          className={styles.background_image}
-          src={"/hero.png"}
+          className={styles.image}
+          src={"/a.jpg"}
           width={250}
           height={250}
         />
-        <button className={styles.check_button}>Check availability</button>
+        <Image
+          className={styles.image}
+          src={"/b.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/c.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/d.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/e.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/f.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/g.jpg"}
+          width={250}
+          height={250}
+        />
+        <Image
+          className={styles.image}
+          src={"/h.jpg"}
+          width={250}
+          height={250}
+        />
       </div>
-    </div>
+    </>
   );
 }
