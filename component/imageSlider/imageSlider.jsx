0b96a9e9818f212d84d2ed111a720aff7e471e52
learnease95 <learnease95@gmail.com>
2024-06-14 10:55:29 -0700

    added image slider

--- component/imageSlider/imageSlider.jsx
+++ component/imageSlider/imageSlider.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import styles from "./ImageSlider.module.css";
+import styles from "./imageSlider.module.css";
 
 const images = [
   "/a.jpg",
